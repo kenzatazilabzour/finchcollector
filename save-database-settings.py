d6508da7335d9bbcd56869f895ff77bba693648e
kenzatazilabzour <kenzatazi17@gmail.com>
2023-09-22 14:57:47 -0700

    Added files

---
+++ save-database-settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'finchcollector',
+    'USER': 'kenzatazilabzour',
+    'PASSWORD': 'WPLOhsCp1t5H',
+    'HOST': 'ep-bitter-math-61863887.us-east-2.aws.neon.tech',
+    'PORT': '5432',
+  }
+}
