985b52bd1c2a1c4095dace3a17a3835a6f2dd28b
kenzatazilabzour <kenzatazi17@gmail.com>
2023-09-25 15:10:44 -0700

    updated part 3

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('finch/', views.finch_index, name='finch_index'),
   path('<int:finch_id>/', views.finch_detail, name='finch_detail'),
-  path('cats/create/', views.FinchCreate.as_view(), name='finch_create'),
-  path('cats/<int:pk>/update/', views.FinchUpdate.as_view(), name='finch_update'),
-  path('cats/<int:pk>/delete/', views.FinchDelete.as_view(), name='finch_delete'),
+  path('finch/create/', views.FinchCreate.as_view(), name='finch_create'),
+  path('finch/<int:pk>/update/', views.FinchUpdate.as_view(), name='finch_update'),
+  path('finch/<int:pk>/delete/', views.FinchDelete.as_view(), name='finch_delete'),
 ]
